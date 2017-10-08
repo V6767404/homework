d7586321f2db5e3eb3cefc970caf3ecb2ccbd665
V6767404 <32267531+V6767404@users.noreply.github.com>
2017-10-08 10:19:10 +0300

    Update Page56Ex1.java

--- part02/Page56Ex1.java
+++ part02/Page56Ex1.java
@@ -1,27 +1,22 @@
 package homework.part02;
-//Создайте программу, которая будет генерировать и выводить на
-//экран вещественное псевдослучайное число из промежутка [-3;3).
-
+//РЎРѕР·РґР°Р№С‚Рµ РїСЂРѕРіСЂР°РјРјСѓ, РєРѕС‚РѕСЂР°СЏ Р±СѓРґРµС‚ РіРµРЅРµСЂРёСЂРѕРІР°С‚СЊ Рё РІС‹РІРѕРґРёС‚СЊ РЅР°
+//СЌРєСЂР°РЅ РІРµС‰РµСЃС‚РІРµРЅРЅРѕРµ РїСЃРµРІРґРѕСЃР»СѓС‡Р°Р№РЅРѕРµ С‡РёСЃР»Рѕ РёР· РїСЂРѕРјРµР¶СѓС‚РєР° [-3;3).
 
 public class Page56Ex1 {
 
 	public static void main(String[] args) {
-		
-		//(max - min + 1) + min  для [min;max]
-		//(int)(Math.random()*(max - min + 1) + min)) - работает в положительном диапозоне
-		//(int)(Math.random()*(max - min + 1)) + min) - работает во всех диапозонах. Какой смысл тогда в первой формуле?
 
-		// ?? не могу понять когда работает формула (max - min + 2) - (min + 1) приведенная на лекции
-		//System.out.println((int)( Math.random() * 8 + 2)); // не работает [-3;3] (3- (-3) +2) - (-3+1)
 		
-		System.out.println((int) (Math.random() * 6) - 3); // [-3;3) -по сути то же что и [-3;2]
-		System.out.println((int) (Math.random() * 6) - 3); 
-		System.out.println((int) (Math.random() * 6) - 3);
-		System.out.println((int) (Math.random() * 6) - 3);
-		System.out.println((int) (Math.random() * 6) - 3);
-	
+		// (int)(Math.random()*(max - min + 1) + min)) - СЂР°Р±РѕС‚Р°РµС‚ РІ РїРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРј РґРёР°РїРѕР·РѕРЅРµ [min;max]
+		// (int)(Math.random()*(max - min + 1)) + min) - СЂР°Р±РѕС‚Р°РµС‚ РІРѕ РІСЃРµС… РґРёР°РїРѕР·РѕРЅР°С… !!
+		// РљР°РєРѕР№ СЃРјС‹СЃР» С‚РѕРіРґР° РІ РїРµСЂРІРѕР№ С„РѕСЂРјСѓР»Рµ? - РќРёРєР°РєРѕРіРѕ!
+		// С„РѕСЂРјСѓР»Р° РґР»СЏ РѕС‚СЂРёС† РґРёР°РїРѕР·РѕРЅР° (max - min + 2) - (min - 1) С‚РѕР¶Рµ РёР·Р±С‹С‚РѕС‡РЅР°
 
-	
+		System.out.println((int) (Math.random() * 6) - 3); // [-3;3) -РїРѕ СЃСѓС‚Рё С‚Рѕ Р¶Рµ С‡С‚Рѕ Рё [-3;2]
+		System.out.println((int) (Math.random() * 6) - 3);
+		System.out.println((int) (Math.random() * 6) - 3);
+		System.out.println((int) (Math.random() * 6) - 3);
+		System.out.println((int) (Math.random() * 6) - 3);
 
 	}
 
